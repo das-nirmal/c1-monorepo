3d85dd7a7e9e94a5a66cf0f139625e51ab396a08
Nirmal <nirmal.kumar@comprotechnologies.com>
2023-08-30 15:18:19 +0530

    base nuxt config update with google font

--- layers/base/components/HelloWorld.vue
+++ layers/base/components/HelloWorld.vue
@@ -1,3 +1,9 @@
 <template>
-  <p>Hello from base layer!</p>
+  <p class="hello-msg">Hello from base layer!</p>
 </template>
+
+<style>
+.hello-msg {
+  font-family: "Mulish", "sans-serif";
+}
+</style>
--- layers/base/nuxt.config.ts
+++ layers/base/nuxt.config.ts
@@ -6,4 +6,14 @@ export default defineNuxtConfig({
       link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
     },
   },
+  modules: [
+    ['@nuxtjs/google-fonts', {
+      families: {
+        'Mulish' : {
+          wght: [200, 300, 400, 600, 700, 800],
+          ital: [300, 600]
+        }
+      }
+    }]
+  ]
 });
