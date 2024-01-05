d153fc0c9c14989ccc0a617bb974440986035876
Nirmal <nirmal.kumar@comprotechnologies.com>
2024-01-05 15:57:49 +0530

    cleanup

--- apps/onboarding/server/api/test.get.ts
+++ apps/onboarding/server/api/test.get.ts
@@ -1,11 +1,7 @@
 export default defineEventHandler(() => {
-  // const appConfig = useAppConfig();
-  // return appConfig.name;
 
   const runtimeConfig = useRuntimeConfig();
-  console.log(runtimeConfig)
-  //console.log(JSON.stringify(runtimeConfig, null, 4))
-  //return runtimeConfig.onboarding.contactSupport.emailAddress;
+  console.log(runtimeConfig);
 
   return runtimeConfig._app.context;
 })
